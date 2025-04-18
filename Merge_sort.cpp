e662a7248f5e6af5dfa7f76f049f9b507cdfc8d1
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-04-18 22:13:47 +0500

    Update Merge_sort.cpp

--- Merge_sort.cpp
+++ Merge_sort.cpp
@@ -8,12 +8,12 @@
      for (int j = 0; j < n2; j++) R[j] = a[m + 1 + j];
      int i = 0, j = 0, k = l;
      while (i < n1 && j < n2) a[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
-//     while (i < n1) a[k++] = L[i++];
-//     while (j < n2) a[k++] = R[j++];
-// }
-// void mergeSort(int a[], int l, int r) {
-//     if (l < r) {
-//         int m = l + (r - l) / 2;
+     while (i < n1) a[k++] = L[i++];
+     while (j < n2) a[k++] = R[j++];
+ }
+ void mergeSort(int a[], int l, int r) {
+     if (l < r) {
+         int m = l + (r - l) / 2;
 //         mergeSort(a, l, m);
 //         mergeSort(a, m + 1, r);
 //         merge(a, l, m, r);
